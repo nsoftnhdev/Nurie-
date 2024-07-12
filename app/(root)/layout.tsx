78c24b3da4119c5cdcd61fe12d99651cc0f91fb4
nsoftnhdev <153261745+nsoftnhdev@users.noreply.github.com>
2024-07-12 21:03:19 +0800

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,24 @@
+
+import StreamVideoProvider from '@/providers/StreamClientProvider'
+import { Metadata } from 'next';
+import React, { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+  title: "NURIE+",
+  description: "Video calling app",
+  icons: {
+    icon:'/icons/logo.svg'
+  }
+};
+
+const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
+  return (
+    <main>
+      <StreamVideoProvider>
+      {children}
+      </StreamVideoProvider>
+    </main>
+  )
+}
+
+export default RootLayout
