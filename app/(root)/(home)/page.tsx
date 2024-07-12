b2ef20155ef7b6bd2be09756f33ca36f38a32d8c
nsoftnhdev <153261745+nsoftnhdev@users.noreply.github.com>
2024-07-13 01:12:33 +0800

    Time zone changes

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,8 +3,8 @@ import MeetingTypeList from '@/components/MeetingTypeList';
 const Home = () => {
   const now = new Date();
 
-  const time = now.toLocaleTimeString('en-MY', {hour: '2-digit', minute: '2-digit'});
-  const date = (new Intl.DateTimeFormat('en-MY', {dateStyle: 'full'})).format(now);
+  const time = now.toLocaleTimeString('en-US', {timeZone: "Asia/Malaysia", hour: '2-digit', minute: '2-digit'});
+  const date = (new Intl.DateTimeFormat('en-US', {dateStyle: 'full'})).format(now);
 
   return (
     <section className="flex size-full flex-col gap-10 text-white">
